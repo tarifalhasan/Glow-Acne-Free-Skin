be42df4e5853d2d3930682a501892f57cf6c00bc
tarifalhasan <tarifalhasan999@gmail.com>
2025-01-13 10:07:39 +0600

    update

--- src/components/pages/home/Hero.tsx
+++ src/components/pages/home/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
           alt="hero"
           className="absolute lg:max-w-[160px] xl:max-w-[260px] 2xl:max-w-[450px] left-[5%] 2xl:left-[-15rem] z-50 hidden lg:block"
           style={{
-            bottom: `calc(36rem - ${Math.min(scrollY * 0.5, 240)}px)`,
+            bottom: `calc(37rem - ${Math.min(scrollY * 0.5, 240)}px)`,
 
             transform: `translateY(${Math.min(scrollY * 0.5, 240)}px)`,
           }}
@@ -43,7 +43,7 @@ const Hero = () => {
 
         <img
           src="/img/hero/hero-product.webp"
-          className="absolute lg:max-w-[200px] right-[5%] 2xl:right-[-15rem] xl:max-w-[300px] 2xl:max-w-[550px] z-50 hidden lg:block"
+          className="absolute lg:max-w-[200px] right-[5%] 2xl:right-[-15rem] xl:max-w-[300px]  2xl:w-[150%] 2xl:max-w-[550px] z-50 hidden lg:block"
           alt="hero"
           style={{
             bottom: `calc(36rem - ${Math.min(scrollY * 0.5, 240)}px)`,
